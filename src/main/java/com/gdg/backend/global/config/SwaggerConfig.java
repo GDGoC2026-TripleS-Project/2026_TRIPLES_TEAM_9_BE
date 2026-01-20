d17154c41f4f6c46a58d487fa4f98c2646872238
woohyuk0428 <whkang03@gmail.com>
2026-01-20 17:06:05 +0900

    #1 feat: swagger ui를 이용할 config

---
+++ src/main/java/com/gdg/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.gdg.backend.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "triples API", version = "v1"),
+        security = @SecurityRequirement(name = "BearerAuth")
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi authApi() {
+        return GroupedOpenApi.builder()
+                .group("Auth API")
+                .pathsToMatch("/api/**","/login","/mypage/**","/signup")
+                .build();
+    }
+
+}
