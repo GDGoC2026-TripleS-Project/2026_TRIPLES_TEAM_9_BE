46aedf751ff2669ae0e3a40c6ac47723281187ac
woohyuk0428 <whkang03@gmail.com>
2026-01-23 01:24:18 +0900

    #1 feat: userService 구현

---
+++ src/main/java/com/gdg/backend/user/service/UserService.java
@@ -0,0 +1,49 @@
+package com.gdg.backend.user.service;
+
+import com.gdg.backend.global.exception.UserNotFoundException;
+import com.gdg.backend.user.domain.User;
+import com.gdg.backend.user.dto.UserInfoResponseDto;
+import com.gdg.backend.user.dto.UserUpdateRequestDto;
+import com.gdg.backend.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    @Transactional(readOnly = true)
+    public UserInfoResponseDto getMyInfo(Long userId){
+        User user = getUser(userId);
+
+        return UserInfoResponseDto.from(user);
+    }
+
+    @Transactional
+    public void updateUser(Long userId, UserUpdateRequestDto request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("유저를 찾을 수 없습니다."));
+
+        if (request.getNickname() != null) {
+            user.updateNickname(request.getNickname());
+        }
+
+    }
+
+
+    @Transactional
+    public void deleteUser(Long userId){
+        User user = getUser(userId);
+
+        userRepository.delete(user);
+    }
+
+    private User getUser(Long userId){
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("사용자를 찾을 수 없습니다."));
+    }
+
+
+}
