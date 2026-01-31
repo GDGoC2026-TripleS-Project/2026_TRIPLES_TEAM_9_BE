98964af4ea3dcc617e5e7333370bf466f4b3cd9d
woohyuk0428 <whkang03@gmail.com>
2026-01-31 14:46:51 +0900

    #13 fix: import 문 최적화

--- src/main/java/com/gdg/backend/api/auth/mindMap/domain/MindMapNode.java
+++ src/main/java/com/gdg/backend/api/auth/mindMap/domain/MindMapNode.java
@@ -3,9 +3,12 @@ package com.gdg.backend.api.auth.mindMap.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -14,9 +17,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -42,8 +43,7 @@ public class MindMapNode {
     @JoinColumn(name = "mind_map_id", nullable = false)
     private MindMap mindMap;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "keyword_id")
+    @Enumerated(EnumType.STRING)
     private Keyword keyword;
 
     @Column(name = "title", nullable = false, length = 80)
