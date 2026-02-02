2703f9738cd6e2db3139b8272ec7a0b0424a9bf5
Choi, Yoora <liveoh387@gmail.com>
2026-02-02 12:11:45 +0900

    #14 chore: 불필요한 import 정리

--- src/main/java/com/gdg/backend/api/global/record/dto/CreateRecordResponseDto.java
+++ src/main/java/com/gdg/backend/api/global/record/dto/CreateRecordResponseDto.java
@@ -3,8 +3,6 @@ package com.gdg.backend.api.global.record.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDate;
-
 @Getter
 @Builder
 public class CreateRecordResponseDto {
