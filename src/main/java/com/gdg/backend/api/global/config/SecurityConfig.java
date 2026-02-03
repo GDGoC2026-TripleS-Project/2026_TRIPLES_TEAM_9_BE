537fbeee81420bf45833a940a75317f5b9a61e3f
woohyuk0428 <whkang03@gmail.com>
2026-02-03 23:15:15 +0900

    #19 feat: 403 forbidden 허용

--- src/main/java/com/gdg/backend/api/global/config/SecurityConfig.java
+++ src/main/java/com/gdg/backend/api/global/config/SecurityConfig.java
@@ -49,7 +49,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/user/**",
                                 "/record/**",
-                                "/mindmaps/**"
+                                "/mindmaps/**",
+                                "/dashboard/**"
                         ).authenticated()
                         .anyRequest().authenticated()
                 )
