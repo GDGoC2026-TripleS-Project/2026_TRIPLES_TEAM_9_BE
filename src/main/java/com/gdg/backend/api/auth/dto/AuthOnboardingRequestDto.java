baa895858936d4a9b1a6cbe950726def31a69996
woohyuk0428 <whkang03@gmail.com>
2026-02-04 17:54:12 +0900

    #21 feat: 요청 값 추가

--- src/main/java/com/gdg/backend/api/auth/dto/AuthOnboardingRequestDto.java
+++ src/main/java/com/gdg/backend/api/auth/dto/AuthOnboardingRequestDto.java
@@ -1,13 +1,30 @@
 package com.gdg.backend.api.auth.dto;
 
+import com.gdg.backend.api.user.domain.LearningField;
+import com.gdg.backend.api.user.domain.LearningGoal;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class AuthOnboardingRequestDto {
     @NotBlank
     private String authToken;
+
     @NotBlank
     private String nickname;
-    //회원가입 시 추가 정보는 여기다가 추ㅏㄱ
+
+    @NotNull
+    private LearningGoal learningGoal;
+
+    @Size(max = 100)
+    private String learningGoalText;
+
+    private List<LearningField> learningFields;
+
+    @Size(max = 200)
+    private String resolution;
 }
