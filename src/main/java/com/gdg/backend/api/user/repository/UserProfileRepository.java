4efb8d37342c96c4949f45d1ba28289549c2ffa5
woohyuk0428 <whkang03@gmail.com>
2026-02-04 17:57:44 +0900

    #21 feat: userProfile repository 구현

---
+++ src/main/java/com/gdg/backend/api/user/repository/UserProfileRepository.java
@@ -0,0 +1,7 @@
+package com.gdg.backend.api.user.repository;
+
+import com.gdg.backend.api.user.domain.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+}
