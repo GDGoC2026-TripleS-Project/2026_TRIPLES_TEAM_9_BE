dacd90833726eac88c82f6a847b30c5194a8ddc6
woohyuk0428 <whkang03@gmail.com>
2026-02-04 17:54:46 +0900

    #21 feat: 학습 목표 enum 추가

---
+++ src/main/java/com/gdg/backend/api/user/domain/LearningGoal.java
@@ -0,0 +1,10 @@
+package com.gdg.backend.api.user.domain;
+
+public enum LearningGoal {
+    JOB,          // 취업
+    CERTIFICATE,  // 자격증
+    PROJECT,      // 프로젝트
+    WORK,         // 업무
+    SELF_GROWTH,  // 자기계발
+    OTHER           // 기타
+}
