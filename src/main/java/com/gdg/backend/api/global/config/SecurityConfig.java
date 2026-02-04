f8474a49ed2c992dde4d74ed3dad1b27ea09f08b
woohyuk0428 <whkang03@gmail.com>
2026-02-04 17:56:30 +0900

    #21 feat: mypage url 접근 허용

--- src/main/java/com/gdg/backend/api/global/config/SecurityConfig.java
+++ src/main/java/com/gdg/backend/api/global/config/SecurityConfig.java
@@ -50,7 +50,8 @@ public class SecurityConfig {
                                 "/user/**",
                                 "/record/**",
                                 "/mindmaps/**",
-                                "/dashboard/**"
+                                "/dashboard/**",
+                                "/my/**"
                         ).authenticated()
                         .anyRequest().authenticated()
                 )
