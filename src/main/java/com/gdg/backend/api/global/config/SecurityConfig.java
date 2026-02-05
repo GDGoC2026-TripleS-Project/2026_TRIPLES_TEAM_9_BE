1287e4e560087d22e2bae85d078cbdb0bc33fdc5
woohyuk0428 <whkang03@gmail.com>
2026-02-05 20:35:17 +0900

    #24 refactor: mypage로 변경

--- src/main/java/com/gdg/backend/api/global/config/SecurityConfig.java
+++ src/main/java/com/gdg/backend/api/global/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                                 "/record/**",
                                 "/mindmaps/**",
                                 "/dashboard/**",
-                                "/my/**"
+                                "/mypage/**"
                         ).authenticated()
                         .anyRequest().authenticated()
                 )
