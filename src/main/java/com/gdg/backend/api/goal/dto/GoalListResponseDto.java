78639c578268cf8f3f7dc9695a045494ba1dae87
Choi, Yoora <liveoh387@gmail.com>
2026-02-10 22:11:14 +0900

    #27 chore: 불필요한 import 정리

--- src/main/java/com/gdg/backend/api/goal/dto/GoalListResponseDto.java
+++ src/main/java/com/gdg/backend/api/goal/dto/GoalListResponseDto.java
@@ -4,8 +4,6 @@ import com.gdg.backend.api.goal.domain.Goal;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
-
 @Getter
 @AllArgsConstructor
 public class GoalListResponseDto {
--- src/main/java/com/gdg/backend/api/goal/repository/TaskRepository.java
+++ src/main/java/com/gdg/backend/api/goal/repository/TaskRepository.java
@@ -3,6 +3,4 @@ package com.gdg.backend.api.goal.repository;
 import com.gdg.backend.api.goal.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface TaskRepository extends JpaRepository<Task, Long> {}
