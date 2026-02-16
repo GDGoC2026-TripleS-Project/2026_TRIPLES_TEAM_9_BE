172d8bd4bc8c8c44a3dad0be88e891945c39bfcf
woohyuk0428 <whkang03@gmail.com>
2026-02-16 18:50:17 +0900

    #33 refactor: 파일 구조 정리로 인한 삭제

--- src/main/java/com/gdg/backend/api/achievements/dto/Section.java
+++
@@ -1,9 +0,0 @@
-package com.gdg.backend.api.achievements.dto;
-
-import java.util.List;
-
-public record Section(
-        String category,
-        String title,
-        List<BadgeItem> badges
-) {}
